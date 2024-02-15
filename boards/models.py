8a081d20daced7cde47e62378431207599218efb
SwanyCastle <seunghwan712@gmail.com>
2024-02-15 11:27:52 +0900

    admin pannel management

--- boards/models.py
+++ boards/models.py
@@ -9,3 +9,10 @@ from django.db import models
 class Board(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+    writer = models.CharField(max_length=30)
+    date = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+    reviews = models.PositiveIntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.title
