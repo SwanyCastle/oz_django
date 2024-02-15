b3ebf9b9ab7330ba456a9ee6335cd2214b935661
SwanyCastle <seunghwan712@gmail.com>
2024-02-15 12:01:07 +0900

    custion admin / common model

--- boards/models.py
+++ boards/models.py
@@ -1,12 +1,8 @@
 from django.db import models
+from common.models import CommonModel
 
-# Create your models here.
 
-# 게시글
-# - title
-# - content
-
-class Board(models.Model):
+class Board(CommonModel):
     title = models.CharField(max_length=30)
     content = models.TextField()
     writer = models.CharField(max_length=30)
--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
-# Register your models here.
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ["name", "description", "age", "gender"]
-    list_filter = ["age", "gender"]
-    search_fields = ["name"]
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "is_business", "grade")
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        # (
+        #     ("Permissions"),
+        #     {
+        #         "fields": (
+        #             "is_active",
+        #             "is_staff",
+        #             "is_superuser",
+        #             "groups",
+        #             "user_permissions",
+        #         ),
+        #     },
+        # ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
