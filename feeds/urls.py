dbd39fe549c5aba30350dd0b830cb4784bed958e
SwanyCastle <seunghwan712@gmail.com>
2024-02-16 09:52:52 +0900

    Part1

--- feeds/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from feeds import views
-
-urlpatterns = [
-    path("", views.show_feed),
-	path("<int:feed_id>/<str:feed_content>", views.one_feed),
-    path("all/", views.all_feed),
-]
